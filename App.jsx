3aa75a1f8ff29d67fa78eb5606035430062fb7bb
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-11-23 18:41:36 -0700

    lab5

--- App.jsx
+++ App.jsx
@@ -72,7 +72,7 @@ function App() {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.jsx</Text> to change this
+            Edit <Text style={styles.highlight}>lab5</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
